9b6ecaf35a2265300c6d4a8b7cd4f050d8329dc7
Khoa Nguyen <nguyenminhdangkhoa9@gmail.com>
2023-10-23 16:00:56 +0700

    adjust [eslint]: add new rules no-restricted-imports

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -37,6 +37,7 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
+    'no-restricted-imports': ['error', { patterns: ['@mui/*/*/*'] }],
   },
   settings: {
     react: {
